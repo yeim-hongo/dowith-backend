2c452bab19a9a2fae1218df6b928b054b3ad69d6
yeongbinim <yeim.dev@gmail.com>
2024-05-29 14:54:46 +0900

    feat: Member 도메인 클래스 작성
    - id : 식별자
    - userId: 서비스 아이디
    - nickname: 닉네임
    - password: 비밀번호

---
+++ src/main/java/imhong/dowith/member/domain/Member.java
@@ -0,0 +1,13 @@
+package imhong.dowith.member.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Member {
+    private Long id;
+    private String userId;
+    private String nickname;
+    private String password;
+}
