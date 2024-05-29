96b0fda26d7294ccdd118b5f89994c6460dfb581
yeongbinim <yeim.dev@gmail.com>
2024-05-29 14:50:23 +0900

    chore: 프로젝트 초기세팅
    - spring boot 프로젝트 생성
    - validation, lombok, test, h2 패키지 의존성 추가

---
+++ src/test/java/imhong/dowith/DowithApplicationTests.java
@@ -0,0 +1,13 @@
+package imhong.dowith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DowithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
