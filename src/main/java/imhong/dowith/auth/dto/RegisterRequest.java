c11eaff9a1935e58252bd5af1d2c0ec4487e83d8
yeongbinim <yeim.dev@gmail.com>
2024-06-12 14:49:14 +0900

    chore: 아이디, 닉네임 제약조건 추가
    - 아이디 조건 추가(영문자) 및 변경(글자수)
    - 닉네임 조건 추가(영,한자) 및 변경(글자수)

--- src/main/java/imhong/dowith/auth/dto/RegisterRequest.java
+++ src/main/java/imhong/dowith/auth/dto/RegisterRequest.java
@@ -9,18 +9,26 @@ import lombok.Getter;
 public class RegisterRequest {
 
     @NotNull(message = "아이디를 입력해주세요.")
-    @Size(min = 4, max = 20, message = "사용자 ID는 4자에서 20자 사이여야 합니다.")
+    @Size(min = 6, max = 12, message = "사용자 ID는 6자에서 12자 사이여야 합니다.")
+    @Pattern(
+        regexp = "^[a-zA-Z0-9_-]{6,12}$",
+        message = "아이디는 영문자, 숫자, 밑줄, 하이픈만 사용 가능합니다."
+    )
     private String userId;
 
     @NotNull(message = "닉네임을 입력해주세요.")
-    @Size(min = 4, max = 20, message = "닉네임은 4자에서 20자 사이여야 합니다.")
+    @Size(min = 1, max = 12, message = "닉네임은 1자에서 12자 사이여야 합니다.")
+    @Pattern(
+        regexp = "^[a-zA-Z0-9가-힣_-]{1,12}$",
+        message = "닉네임은 영문자, 숫자, 밑줄, 하이픈, 한글만 사용 가능합니다."
+    )
     private String nickname;
 
     @NotNull(message = "비밀번호를 입력해주세요.")
     @Size(min = 8, max = 20, message = "비밀번호는 8자에서 20자 사이여야 합니다.")
     @Pattern(
-        regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=!*]).{8,20}$",
-        message = "비밀번호는 8자 이상 20자 이하이며, 최소 하나의 대문자, 소문자, 숫자, 특수문자를 포함해야 합니다."
+        regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&~+=?!*_-])(?!.*\\\\s).{8,20}$",
+        message = "비밀번호는 최소 하나의 대문자, 소문자, 숫자, 특수문자(@#$%^&~+=?!*_-)를 포함해야 하며, 공백을 포함할 수 없습니다."
     )
     private String password;
 }
